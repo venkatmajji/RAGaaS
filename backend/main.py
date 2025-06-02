befa369cc0b2a173db19f19c7ca8371bdf26b88d
venkatmajji <52803952+venkatmajji@users.noreply.github.com>
2025-06-02 09:11:55 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ragaas-ui.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
