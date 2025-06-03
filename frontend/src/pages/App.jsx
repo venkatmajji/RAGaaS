8a479bdee8f2a30ec05ea62b77b4cfe43604969b
venkatmajji <52803952+venkatmajji@users.noreply.github.com>
2025-06-03 10:46:42 -0400

    Update App.jsx

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "../../supabaseClient";
 import LoginModal from "./components/LoginModal";
 
 export default function App() {
